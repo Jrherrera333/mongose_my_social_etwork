52e298c20c171387019f5920aa827b34585b1e12
Jose Herrera <herrera.joserafael@gmail.com>
2023-09-17 19:10:03 -0400

    created user and thought models

---
+++ models/Thought.js
@@ -0,0 +1,74 @@
+const { Schema, Types } = require('mongoose');
+const { Thought } = require('.');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      // Set default value to the current timestamp
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Thought is Required',
+      minlenght: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //use a getter method to format the timestamp on query
+      get: (timestamp) => dateformat(timestamp),
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    // array of nested documents created with the reactionSchema
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
